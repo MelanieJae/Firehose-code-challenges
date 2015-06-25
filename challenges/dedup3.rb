4b2b2e9e03843244cc4ab33bc952907918835568
MelanieJae <mtaurus575@gmail.com>
2015-06-25 00:43:26 +0000

    dedup3

--- challenges/dedup3.rb
+++ challenges/dedup3.rb
@@ -4,6 +4,7 @@ public
 #leaves only nth or last dupe, whichever comes first
 def dedup_nth_dup(arr, n)
 	@arr = arr
+	@dupcountarray = []
 	#check for duplicates
 	
 	#loop that executes output of dupe depending on what n is
@@ -25,43 +26,48 @@ def dedup_nth_dup(arr, n)
 			@arr.uniq.each_index do |ui|	
 
 				# dup counter array: each element is counter for number of instances of element ui in the unique array
-				# that are present in the original array 
+				# that are present in the original array. 
 				# e.g. 
 				# original array = [1,1,1,2,4,5,6,7,7], index oi
 				# array.uniq = [1,2,4,5,6,7], index ui
 				# dupcounterarray = [3,1,1,1,1,2], index di
 
 				@dupcountarray[ui] = 0
-				@arr.each_index do |oi|
-					if @arr[oi] == @arr.uniq[ui]
-						@dupcountarray[ui] += 1
-					#store indeces of last counter element
-					#interrupt loop when any counter array element reaches n and take note of index oi at that point?
-					#finish incrementing counter array and for those orig array elements whose counter doesn't go
-					#above 1 spit those out as is and any counter >1 but less than n take note of index of that dupe
-					# and return that as the index for the dupe i want in the final array.	
-					end	
+			end
+				@arr.uniq.each_index do |ui|	
+					@arr.each_index do |oi|
+						if @arr[oi] == @arr.uniq[ui]
+							@dupcountarray[ui] += 1
+							
+					#store/associate last counter element for each element of dupcountarray
+					#interrupt loop when any counter array element reaches n and take note of index oi at that point
+					# as the final index for the dupe I want?
+					#finish incrementing counter array and for those uniq array elements whose counter doesn't go
+					#above 1, spit those out as is with index and any counter >1 but less than n, take note of index
+					# of that dupe and return that as the index for the dupe i want in the final array?	
+						end	
+					end
+				end	
+					
+				
+				
+					@arr.each_index do |oi|
+						@dupcountarray.each do |di|
+							# prints two columns
+							# first col. is index of either the element if it's unique in the original array
+							# or the index of the nth or last dupe of a number in the array, whichever comes first
+							# second col. is the duplicated element.
 
-					@dupcountarray.each do |di|
-									# prints two columns
-									# first col. is index of either the element if it's unique in the original array
-									# or the index of the nth or last dupe of a number in the array, whichever comes first
-									# second col. is the duplicated element.
-
-
-									if di >= n
-										puts "#{oi}, #{@arr[oi]}"
-									elsif di < n
-										li = @arr.rindex {|ui| element == @arr[ui]}
-										puts "#{li}, #{@arr[ui]}"
-									end
-								end
-							end
+							 if di > 1
+							  	puts "#{@arr.rindex { |x| x == @arr[oi]}}, #{@arr[oi]}"
+							 end
 						end
 					end
+				
+			
 		end
 end
 
 array = []
-array.dedup_nth_dup([1,1,1,2,4,5,6,7,7], 3)
+array.dedup_nth_dup([1,1,1,2,2,5,6,7,7,8], 3)
 
